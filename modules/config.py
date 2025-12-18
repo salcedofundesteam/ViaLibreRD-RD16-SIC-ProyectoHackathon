1a0e45e6f4dfb23e92347865070d6144fa113a1a
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2025-12-17 23:26:23 -0400

    chore: configuracion del proyecto

---
+++ modules/config.py
@@ -0,0 +1,27 @@
+import os
+
+HAARCASCADE_DIR = "haarcascades"
+VIDEO_DIR = "videoprueba"
+DATA_DIR = "data"
+
+FULLBODY_CASCADE_PATH = os.path.join(HAARCASCADE_DIR, "haarcascade_fullbody.xml")
+CAR_CASCADE_PATH = os.path.join(HAARCASCADE_DIR, "cars.xml")
+VIDEO_PATH = os.path.join(VIDEO_DIR, "autos.mp4")
+DATA_FILE_PATH = os.path.join(DATA_DIR, "datos_trafico.csv")
+
+ROJO = "ROJO"
+VERDE = "VERDE"
+
+# Email Configuration
+EMAIL_SENDER = "josiaspegueroltjnc@gmail.com"
+EMAIL_PASSWORD = "kfcb aktn qbbx lrxy"
+EMAIL_RECIPIENT = "allmusiclir@gmail.com"
+
+# Dominican Republic Locations
+DOMINICAN_LOCATIONS = [
+    "Av. 27 de Febrero",
+    "Av. John F. Kennedy",
+    "Autopista Duarte",
+    "Av. Winston Churchill",
+    "Av. Las Américas"
+]
